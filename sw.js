8f68b46e4083a4f70aaf7afd0d7bf3432aab1780
studio-memo <kamome.a.b.c@gmail.com>
2026-05-09 23:37:45 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-self.addEventListener("install", () => {
+self.addEventListener("install", function (event) {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((key) => caches.delete(key)))
-    )
-  );
+self.addEventListener("activate", function (event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(fetch(event.request));
 });
