f245dc77a099010c2f236645307c482838ec8d39
studio-memo <kamome.a.b.c@gmail.com>
2026-05-09 23:08:12 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
+const CACHE_NAME = "today-memo-v1";
 
+self.addEventListener("install", function (event) {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(fetch(event.request));
+});
